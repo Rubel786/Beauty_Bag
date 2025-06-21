a615a76a94f3882033dd14bcde36b2f1cca18276
Rubel Shaikh <jaipalreddy@PClient25.local>
2025-06-21 13:59:39 +0530

    WIP

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,17 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'cart/model/card_model.dart';
 import 'login/view/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'firebase_options.dart'; // generated using FlutterFire CLI
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  print("Handling background message: ${message.messageId}");
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,11 +29,57 @@ void main() async {
     debugPrint("Failed to load .env file: $e");
   }
 
+  // Initialize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // Crashlytics - catch all Flutter errors
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+
+  // Messaging - background messages handler
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    initFCM();
+  }
+
+  void initFCM() async {
+    FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+    // Request permission (iOS)
+    NotificationSettings settings = await messaging.requestPermission();
+    print('User granted permission: ${settings.authorizationStatus}');
+
+    // Get FCM Token
+    String? token = await messaging.getToken();
+    print("FCM Token: $token");
+
+    // Foreground message handling
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Foreground Message: ${message.notification?.title}');
+      print('Foreground Message: ${message.notification?.body}');
+    });
+
+    // Message opened from terminated state
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print('Notification clicked!');
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,5 +100,6 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 
 
